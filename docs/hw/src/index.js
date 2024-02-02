76302d1084906204c6d28d445bf75cf6ff0fea22
grscheller <geoffrey@scheller.com>
2024-02-02 06:50:09 -0700

    Beginning JS thrashing to get hello-world-js-ext.html to work
    - need to refine my JS & TS web searches
      - content I am finding geared to
        - script kiddies
        - bootcamp graduates
    - I hate you Netscape, why did you have to invent JavaScript?
    - the world would have been better if something like Scheme was used
    - another problem: browsers were made too tolerant to mistakes

--- docs/hw/src/index.js
+++ docs/hw/src/index.js
@@ -1,13 +1,7 @@
-function displayArea() {
-  <p id="hw">
-    Greeting to be displayed here.
-  </p>
-}
-
 function hello() {
-  <button type="button" onClick='document.getElementById("hw").innerHTML =
-      "Hello, World!"'>Click the button</button>
+  <button type="button" onClick='document.getElementById("hw").innerHTML = "Hello, World!"'>
+     Click the button
+  </button>
 }
 
-displayArea();
 hello();
