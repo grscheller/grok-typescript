787731d262890ff5e5cb1386f3f154480b5a0413
grscheller <geoffrey@scheller.com>
2024-02-08 17:06:05 -0700

    Got docs/date/scripts/now.js working
    - coworker Pavin told me to pass array instead of individual values
      - do not know why this made a difference
    - also made hours, minutes, seconds, date and month display as 2 digits
      - had to add one to month since getMonth method is 0 based

--- docs/date/scripts/now.js
+++ docs/date/scripts/now.js
@@ -1,17 +1,25 @@
-function updateNow(currDateDisp, currTimeDisp) {
-  const now = new Date();
-  const month = now.getMonth()
-  const date = now.getDate()
-  const year = now.getFullYear()
-  const hour = now.getHours()
-  const min = now.getMinutes()
-  const sec = now.getseconds()
+function updateNow(disps) {
+    const now = new Date();
 
-  const dateStr = 'Current date: <b>' + month + '/' + date + '/' + year + '</b>'
-  const timeStr = 'Current time: <b>' + hour + ':' + min + ':' + sec + '</b>'
+    const yr4 = now.getFullYear()
+    const mon = now.getMonth() + 1
+    const dom = now.getDate()
+    const hrs = now.getHours()
+    const min = now.getMinutes()
+    const sec = now.getSeconds()
 
-  currDateDisp.innerHTML = dateStr;
-  currTimeDisp.innerHTML = timeStr;
+    const yr4Str = yr4.toString()
+    const monStr = mon > 9 ? mon.toString() : '0' + mon.toString()
+    const domStr = dom > 9 ? dom.toString() : '0' + dom.toString()
+    const hrsStr = hrs > 9 ? hrs.toString() : '0' + hrs.toString()
+    const minStr = min > 9 ? min.toString() : '0' + min.toString()
+    const secStr = sec > 9 ? sec.toString() : '0' + sec.toString()
+
+    const dateStr = 'Current date: <b>' + monStr + '/' + domStr + '/' + yr4Str + '</b>'
+    const timeStr = 'Current time: <b>' + hrsStr + ':' + minStr + ':' + secStr + '</b>'
+
+    disps[0].innerHTML = dateStr;
+    disps[1].innerHTML = timeStr;
 }
 
 const currDateDisplay = document.createElement('p');
@@ -22,7 +30,9 @@ currTimeDisplay.innerHTML = 'Current time:';
 
 const nowButton = document.createElement("button");
 nowButton.textContent = 'Update Now';
-nowButton.addEventListener('click', () => updateNow(currDateDisplay, currTimeDisplay));
+
+// const displays = [currDateDisplay, currTimeDisplay];
+nowButton.addEventListener('click', () => updateNow([currDateDisplay, currTimeDisplay]));
 
 document.body.appendChild(nowButton)
 document.body.appendChild(currDateDisplay)
