fddf0aec582b1875da1f24078928e6a2bb15aee4
grscheller <geoffrey@scheller.com>
2025-03-20 12:56:34 -0600

    Created hw_service.js
    - hw_service.ts -> hw_service.js
      - take former in typescript directions
      - take latter in javascript directions

---
+++ projects/hw/hw_service.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env -S node --experimental-transform-types
+
+import { parseArgs } from "node:util";
+import * as http from "http";
+
+const parms = {
+  options: {
+    name: {
+      type: "string",
+      short: "n",
+    },
+    log: {
+      type: "boolean",
+      short: "l",
+    },
+    logfile: {
+      type: "string",
+    },
+  },
+};
+
+const args = parseArgs(parms);
+
+var server = http.createServer(function (_, res) {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World!\n");
+});
+
+server.listen(8007, "localhost");
+console.log(args);
